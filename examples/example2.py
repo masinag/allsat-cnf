590a24d2f1859569849a5b673ce661babf4a7d28
giuspek <giuseppe.spallitta@unitn.it>
2022-11-15 21:10:22 +0100

    Forgetting example2

--- examples/example2.py
+++ examples/example2.py
@@ -7,13 +7,20 @@ boolean_atoms = list(boolean_atoms)[:10]
 
 def random_formula(depth):
     if depth == 0:
-        return random.choice(boolean_atoms)
-    operator = random.choice([Not, Or, And])
+        operator = random.choice([Or, Not])
+        if operator is Or:
+            return random.choice(boolean_atoms)
+        else:
+            return Not(random.choice(boolean_atoms)) 
+    #operator = random.choice([Or, And, Iff, Not])
+    #operator = random.choice([Or, And, Not])
+    operator = random.choice([Or, And, Iff])
     if operator is Not:
         return Not(random_formula(depth - 1))
     left = random_formula(depth - 1)
     right = random_formula(depth - 1)
     return operator(left, right)
 
-formula = random_formula(10)
-make_example(formula)
+for i in range(1,10):
+    formula = random_formula(15)
+    make_example(formula)
