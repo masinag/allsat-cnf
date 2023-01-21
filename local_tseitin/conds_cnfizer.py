f9188719244d2a38fc3a5c3016e70658dfcdc043
giuspek <giuseppe.spallitta@unitn.it>
2023-01-21 19:26:21 +0100

    new CNF-SHARED cnfizer

--- local_tseitin/conds_cnfizer.py
+++ local_tseitin/conds_cnfizer.py
@@ -417,3 +417,264 @@ class LocalTseitinCNFizerConds(LocalTseitinCNFizer):
         # cnf = And(assertions)
         assert self.is_cnf(cnf), cnf.serialize()
         return cnf
+
+
+
+class LocalTseitinCNFizerShared(LocalTseitinCNFizer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hash_set = dict()
+        self.activators = dict()
+
+    def lt_pol(self, formula, count):
+        if self.verbose:
+            print("{}local_tseitin_rec({})".format(
+                "--" * count, formula))
+
+        # CASO BASE 1: formula monovariabile
+        if self.is_literal(formula):
+            return [([Bool(True)], 0)], formula, formula, []
+
+        left, right = formula.args()
+        cnf1, S1, P1, cnd1 = self.lt_pol(left, count + 1)
+        cnf2, S2, P2, cnd2 = self.lt_pol(right, count + 1)
+        # CASO BASE 2: already seen formula
+        if (S1, S2, formula.node_type()) in self.hash_set:
+            S, P = self.hash_set[(S1, S2, formula.node_type())]
+            P_new = self._new_polarizer()
+            self.activators[(S1, S2, formula.node_type())].append(P_new)
+            return [], S, P_new, []
+        if (S2, S1, formula.node_type()) in self.hash_set:
+            S, P = self.hash_set[(S2, S1, formula.node_type())]
+            P_new = self._new_polarizer()
+            self.activators[(S2, S1, formula.node_type())].append(P_new)
+            return [], S, P_new, []
+
+        # CASO NOT(AND) PER AIG
+        S = self._new_label()
+        P = self._new_polarizer()
+        clauses = []
+        conditions = []
+            
+        # CASO BASE 2: leaf operator
+        if self.is_literal(left) and self.is_literal(right):
+            if formula.is_or():
+                # (S <-> S1 v S2)
+                if self.verbose:
+                    print("{} -> ({} <-> {} or {})".format(P, S, S1, S2))
+                clauses.append(([Not(P), Not(S), S1, S2], self.guards))
+                clauses.append(([Not(P), S, Not(S1)], self.guards))
+                clauses.append(([Not(P), S, Not(S2)], self.guards))
+            if formula.is_and():
+                # (S <-> S1 ^ S2)
+                if self.verbose:
+                    print("{} -> ({} <-> {} and {})".format(P, S, S1, S2))
+                clauses.append(([Not(P), S, Not(S1), Not(S2)], self.guards))
+                clauses.append(([Not(P), Not(S), S1], self.guards))
+                clauses.append(([Not(P), Not(S), S2], self.guards))
+            if formula.is_iff():
+                # (S <-> S1 <-> S2)
+                if self.verbose:
+                    print("{} -> ({} <-> {} <-> {})".format(P, S, S1, S2))
+                clauses.append(([Not(P), Not(S), S1, Not(S2)], self.guards))
+                clauses.append(([Not(P), Not(S), S2, Not(S1)], self.guards))
+                clauses.append(([Not(P), S, S1, S2], self.guards))
+                clauses.append(([Not(P), S, Not(S2), Not(S1)], self.guards))
+            self.hash_set[(S1, S2, formula.node_type())] = (S,P) 
+            self.activators[(S1, S2, formula.node_type())] = [P]
+            return clauses, S, P, []
+
+        if formula.is_or():
+            # S <-> S1 v S2
+            if self.verbose:
+                print("{} -> ({} <-> {} or {})".format(P, S, S1, S2))
+            clauses.append(([Not(P), Not(S), S1, S2], self.guards))
+            clauses.append(([Not(P), S, Not(S1)], self.guards))
+            clauses.append(([Not(P), S, Not(S2)], self.guards))
+            # S -> -S1 v -S2
+            if self.verbose:
+                print("{} -> ({} -> {} xor {})".format(P, S, P1, P2))
+            conditions.append(([Not(P), Not(S), Not(P1), Not(P2)], self.guards))
+            conditions.append(([Not(P), Not(S), P1, P2], self.guards))
+            if not self.is_literal(left):
+                if self.verbose:
+                    print("{} and {} -> {}".format(P, Not(S), P1))
+                conditions.append([[Not(P), S, P1], self.guards])
+                if self.verbose:
+                    print("{} and {} and {} -> {}".format(P, S, Not(S2), P1))
+                conditions.append([[Not(P), Not(S), S2, P1], self.guards])
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P1)))
+                conditions.append([[P, Not(P1)], self.guards])
+            if not self.is_literal(right):
+                if self.verbose:
+                    print("{} and {} -> {}".format(P, Not(S), P2))
+                conditions.append([[Not(P), S, P2], self.guards])
+                if self.verbose:
+                    print("{} and {} and {} -> {}".format(P, S, Not(S1), P2))
+                conditions.append([[Not(P), Not(S), S1, P2], self.guards])
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P2)))
+                conditions.append([[P, Not(P2)], self.guards])
+        if formula.is_and():
+            # S <-> S1 and S2
+            if self.verbose:
+                print("{} -> ({} <-> {} and {})".format(P, S, S1, S2))
+            clauses.append(([Not(P), S, Not(S1), Not(S2)], self.guards))
+            clauses.append(([Not(P), Not(S), S1], self.guards))
+            clauses.append(([Not(P), Not(S), S2], self.guards))
+            # -S -> S1 v S2
+            if P1 == S1:
+                P1 = Not(P1)
+            if P2 == S2:
+                P2 = Not(P2)
+            if self.verbose:
+                print("{} -> ({} -> {} xor {})".format(P, Not(S), P1, P2))
+            conditions.append(([Not(P), S, Not(P1), Not(P2)], self.guards))
+            conditions.append(([Not(P), S, P1, P2], self.guards))
+
+            if not self.is_literal(left):
+                if self.verbose:
+                    print("{} and {} -> {}".format(P, S, P1))
+                conditions.append([[Not(P), Not(S), P1], self.guards])
+                if self.verbose:
+                    print("{} and {} and {} -> {}".format(P, Not(S), S2, P1))
+                conditions.append([[Not(P), S, Not(S2), P1], self.guards])
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P1)))
+                conditions.append([[P, Not(P1)], self.guards])
+            if not self.is_literal(right):
+                if self.verbose:
+                    print("{} and {} -> {}".format(P, S, P2))
+                conditions.append([[Not(P), Not(S), P2], self.guards])
+                if self.verbose:
+                    print("{} and {} and {} -> {}".format(P, Not(S), S1, P2))
+                conditions.append([[Not(P), S, Not(S1), P2], self.guards])
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P2)))
+                conditions.append([[P, Not(P2)], self.guards])
+        if formula.is_iff():
+            # S <-> S1 <-> S2
+            if self.verbose:
+                print("{} -> ({} <-> {} <-> {})".format(P, S, S1, S2))
+            clauses.append(([Not(P), Not(S), S1, Not(S2)], self.guards))
+            clauses.append(([Not(P), Not(S), S2, Not(S1)], self.guards))
+            clauses.append(([Not(P), S, S1, S2], self.guards))
+            clauses.append(([Not(P), S, Not(S2), Not(S1)], self.guards))
+
+            if not self.is_literal(left):
+                if self.verbose:
+                    print("{} -> {}".format(P, P1))
+                conditions.append(([Not(P), P1], self.guards))
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P1)))
+                conditions.append([[P, Not(P1)], self.guards])
+            if not self.is_literal(right):
+                if self.verbose:
+                    print("{} -> {}".format(P, P2))
+                conditions.append(([Not(P), P2], self.guards))
+                if self.verbose:
+                    print("{} -> {}".format(Not(P), Not(P2)))
+                conditions.append([[P, Not(P2)], self.guards])
+
+        clauses.reverse()
+        conditions.reverse()
+
+        if not self.is_literal(left):
+            for c in cnf1:
+                clauses.append(c)
+        if not self.is_literal(right):
+            for c in cnf2:
+                clauses.append(c)
+        
+        for c in cnd1:
+            conditions.append(c)
+        for c in cnd2:
+            conditions.append(c)
+        
+        self.hash_set[(S1, S2, formula.node_type())] = (S,P) 
+        self.activators[(S1, S2, formula.node_type())] = [P]
+        return clauses, S, P, conditions
+
+    def convert_as_formula(self, formula):
+        if self.verbose:
+            print("Input formula:", formula.serialize())
+        formula = self.preprocessor.convert_as_formula(formula)
+        if self.verbose:
+            print("Preprocessed formula:", formula.serialize())
+
+        # self.original_symb = S
+        if self.verbose:
+            print("Recursive calls to local_tseitin(formula, conds, symbol, polarity):")
+
+        # cnf = self.local_tseitin_rec(formula, set(), S, 0, P)
+        clauses, S, P, conditions = self.lt_pol(formula, 0)
+
+        #clauses.reverse()
+        #conditions.reverse()
+
+        for f in self.activators:
+            if len(self.activators[f]) > 1:
+                P_new = self._new_polarizer()
+                if self.verbose:
+                    print("SWAP {} with {}".format(self.activators[f][0], P_new))
+                    print("ADD And(Not({})) -> {}".format(self.activators[f], Not(P_new)))
+                for c in clauses:
+                    for i in range(len(c[0])):
+                        if c[0][i] == self.activators[f][0]:
+                            c[0][i] = P_new
+                        elif c[0][i] == Not(self.activators[f][0]):
+                            c[0][i] = Not(P_new)
+                for c in conditions:
+                    if c[0][0] == self.activators[f][0]:
+                        c[0][0] = P_new
+                    elif c[0][0] == Not(self.activators[f][0]):
+                        c[0][0] = Not(P_new)
+                conditions.append(([p for p in self.activators[f]] + [Not(P_new)], self.guards))
+                for p in self.activators[f]:
+                    conditions.append(([Not(p), P_new], self.guards))
+            
+
+        clauses.append((S, 0))
+        clauses.append((P, 0))
+
+        # print("SIZE NEW FORMULA:", len(cnf))
+        cnf = conditions + clauses
+
+        
+
+        """
+        for clause in clauses:
+            clause = clause[0]
+            _clause = []
+            for lit in clause:
+                if lit == S or lit == P:
+                    # ignore clauses as !S -> ...
+                    _clause.clear()
+                    break
+                elif (lit.is_not() and lit.arg(0) == S) or (lit.is_not() and lit.arg(0) == P):
+                    # convert S -> l1 v ... lk into l1 v ... lk
+                    continue
+                else:
+                    _clause.append(lit)
+            if _clause:
+                cnf.append(_clause)
+        """
+        cnf = And([Or(c[0]) for c in cnf])
+                
+
+        if self.verbose:
+            print()
+            print("Encoded clauses:")
+            for el in cnf.args():
+                print(el)
+            print()
+
+        # cnf = substitute(cnf, {S: Bool(True)})
+        # cnf = simplify(cnf)
+
+        # print("Clauses:\n", pformat(assertions))
+        # cnf = And(assertions)
+        assert self.is_cnf(cnf), cnf.serialize()
+        return cnf
