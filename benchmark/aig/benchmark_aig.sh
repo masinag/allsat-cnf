0e6722ce5d6e708b5b63c78a282d0cc20dd065b3
masinag <gabriele.masina@virgilio.it>
2023-02-21 14:42:02 +0100

    Aig benchmark: discard those with <= 1 model

--- benchmark/aig/benchmark_aig.sh
+++ benchmark/aig/benchmark_aig.sh
@@ -4,20 +4,20 @@ DIR=aig-bench
 DATA_URL=http://fmv.jku.at/aiger/smtqfbv-aigs.7z
 
 # check if p7zip is installed
-if ! which py7zr > /dev/null; then
-    echo "py7zr not found: please install it by running 'pip install py7zr'"
-    exit 1
+if ! which py7zr >/dev/null; then
+  echo "py7zr not found: please install it by running 'pip install py7zr'"
+  exit 1
 fi
-
-BOOL=$1
-DEPTH=$2
 
 REPETITIONS=""
 TIMEOUT=600
 
 archname=$(basename $DATA_URL)
 dirname=$DIR/data/${archname%.*}
+# benchmarks with more than 1 model
+IFS=$'\n' read -d '' -r -a relevant_benchmarks <"relevant_benchmarks.txt"
 
+echo ${relevant_benchmarks[*]}
 
 # check if data DIR/data exists
 if [ ! -d $dirname ]; then
@@ -28,20 +28,20 @@ if [ ! -d $dirname ]; then
   echo "Downloading data from $DATA_URL..."
   wget -O $tmpdata $DATA_URL
   echo "Extracting data to $dirname..."
-#  tar xzf $tmpdata -C $dirname
   py7zr x $tmpdata $dirname
   rm $tmpdata
 
-
 fi
-
 
 testdirname="$dirname-test"
 if [ ! -d $testdirname ]; then
-	mkdir $testdirname
-	for f in $(ls $dirname | grep test); do
-		ln -s $(realpath $dirname/$f) $testdirname/$f
-	done
+  mkdir $testdirname
+  for f in $(ls $dirname | grep test); do
+    # shellcheck disable=SC2076
+    if [[ " ${relevant_benchmarks[*]} " =~ " ${f} " ]]; then
+      ln -s $(realpath $dirname/$f) $testdirname/$f
+    fi
+  done
 fi
 
 mkdir -p $DIR/results $DIR/plots
@@ -50,9 +50,9 @@ for dir in $(ls -d $DIR/data/* | grep test); do
   res_dir=$(sed "s+data+results+g" <<<$dir)
   mkdir -p "$res_dir"
   echo $dir
-  for mode in AUTO NNF_AUTO CND NNF_CND POL NNF_POL; do
+  for mode in NNF_POL POL; do
     python3 ../evaluate.py "$dir" -m $mode -o "$res_dir" $REPETITIONS --timeout $TIMEOUT
   done
 done
 
-python3 ../plot.py $DIR/results/*b${BOOL}_d${DEPTH}* -o $DIR/plots
+python3 ../plot.py $DIR/results/*test* -o $DIR/plots
