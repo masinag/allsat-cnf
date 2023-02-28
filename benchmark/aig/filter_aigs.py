4bedc6831d048f27e1962386e5fc7987f5a520b1
masinag <gabriele.masina@virgilio.it>
2023-02-28 21:52:23 +0100

    make filter_aigs script write relevant benchmarks file

--- benchmark/aig/filter_aigs.py
+++ benchmark/aig/filter_aigs.py
@@ -1,7 +1,7 @@
 import os
 
 from tqdm import tqdm
-from pysmt.shortcuts import is_sat
+from pysmt.shortcuts import is_sat, get_model, Or, Not, And
 
 from benchmark.utils.fileio import read_formula_from_file
 from benchmark.utils.run import run_with_timeout
@@ -19,11 +19,6 @@ def get_relevant_benchmarks(data_dir):
     return relevant_benchmarks
 
 
-def should_skip_by_name(filename):
-    basename = os.path.basename(filename)
-    return basename.startswith("count") or basename.startswith("mul")
-
-
 def is_relevant_benchmark(filename):
     if should_skip_by_name(filename):
         return False
@@ -32,16 +27,34 @@ def is_relevant_benchmark(filename):
     if not (0 < i <= MAX_VARS):
         return False
     try:
-        phi = read_formula_from_file(filename)
-        return is_sat_or_timeout(phi)
+        return has_at_least_two_models(filename)
     except (RecursionError, TimeoutError):
         return False
+
+
+def should_skip_by_name(filename):
+    basename = os.path.basename(filename)
+    return basename.startswith("count") or basename.startswith("mul")
 
 
 def get_header(filename):
     with open(filename, "rb") as f:
         header = f.readline().decode("ascii")
     return header
+
+
+def has_at_least_two_models(filename):
+    phi = read_formula_from_file(filename)
+    model = get_model_or_timeout(phi)
+    if model is None:
+        return False
+    conflict_clause = Or([Not(a) for a in model])
+    phi = And(phi, conflict_clause)
+    return is_sat_or_timeout(phi)
+
+
+def get_model_or_timeout(phi):
+    return run_with_timeout(get_model, 600, phi)
 
 
 def parse_header(header):
@@ -55,23 +68,18 @@ def is_sat_or_timeout(phi):
     return run_with_timeout(is_sat, 600, phi)
 
 
-def create_subdir_for_relevant_benchmarks(relevant_benchmarks, relevant_benchmarks_dir):
-    if os.path.exists(relevant_benchmarks_dir):
-        print(f"Directory {relevant_benchmarks_dir} already exists")
-        return
-    os.mkdir(relevant_benchmarks_dir)
-    for filename in relevant_benchmarks:
-        basename = os.path.basename(filename)
-        dest_filename = os.path.join(relevant_benchmarks_dir, basename)
-        os.symlink(filename, dest_filename)
+def write_relevant_benchmarks(relevant_benchmarks, filename):
+    with open(filename, "w") as f:
+        f.writelines(map(os.path.basename, relevant_benchmarks))
 
 
 def main():
     here = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(here, "aig-bench/data/smtqfbv-aigs/")
     relevant_benchmarks = get_relevant_benchmarks(data_dir)
-    relevant_benchmarks_dir = os.path.join(here, "aig-bench/data/smtqfbv-aigs-small/")
-    create_subdir_for_relevant_benchmarks(relevant_benchmarks, relevant_benchmarks_dir)
+    print(f"Found {len(relevant_benchmarks)} relevant benchmarks")
+    relevant_benchmarks_file = os.path.join(here, "small-benchmarks.txt")
+    write_relevant_benchmarks(relevant_benchmarks, relevant_benchmarks_file)
 
 
 if __name__ == '__main__':
