108d38d6001724e3a74256e90967c8c7b6dfb317
masinag <gabriele.masina@virgilio.it>
2023-03-06 22:47:26 +0100

    Bugfix deadlock on run_with_timeout (2)

--- benchmark/utils/run.py
+++ benchmark/utils/run.py
@@ -1,27 +1,28 @@
-from multiprocessing import Queue, get_context
+from multiprocessing import get_context
+from queue import Empty
 
 import psutil as psutil
 
 
 def wrap_fn(fn, q, *args, **kwargs):
-    q.put(fn(*args, **kwargs))
+    res = fn(*args, **kwargs)
+    q.put(res, block=True)
 
 
 def run_with_timeout(fn, timeout, *args, **kwargs):
-    q = Queue()
-    timed_proc = get_context("spawn").Process(
+    ctx = get_context("spawn")
+    q = ctx.Queue()
+    timed_proc = ctx.Process(
         target=wrap_fn,
         args=[fn, q, *args],
         kwargs=kwargs,
     )
     timed_proc.start()
-    timed_proc.join(timeout)
-    if timed_proc.is_alive():
+    try:
+        return q.get(block=True, timeout=timeout)
+    except Empty:
         kill_process_and_children(timed_proc)
         raise TimeoutError("Process was killed due to timeout")
-    elif q.empty():
-        raise TimeoutError("Process was killed due to exceeding resources")
-    return q.get(block=False)
 
 
 def kill_process_and_children(timed_proc):
