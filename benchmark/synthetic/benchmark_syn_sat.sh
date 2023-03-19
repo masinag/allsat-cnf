f33fe00d91f119774a19af62bf6f0d714d657afe
masinag <gabriele.masina@virgilio.it>
2023-03-19 09:56:37 +0100

    Add scripts to run only sat-check (2)

--- benchmark/synthetic/benchmark_syn_sat.sh
+++ benchmark/synthetic/benchmark_syn_sat.sh
@@ -5,7 +5,7 @@ BOOL=$1
 DEPTH=$2
 
 for dir in $(ls -d $DIR/data/*b${BOOL}_d${DEPTH}*); do
-  res_dir=$(sed "s+data+results+g" <<<$dir)
+  res_dir=$(sed "s+data+results-sat+g" <<<$dir)
   mkdir -p "$res_dir"
   for mode in LAB POL NNF_POL NNF_MUTEX_POL LABELNEG_POL; do
     python3 ../evaluate.py "$dir" -m $mode -o "$res_dir" --sat
--- benchmark/wmi/benchmark_wmi_sat.sh
+++ benchmark/wmi/benchmark_wmi_sat.sh
@@ -3,7 +3,7 @@
 DIR=wmi-bench
 
 for dir in $(ls -d $DIR/data/*); do
-  res_dir=$(sed "s+data+results+g" <<<$dir)
+  res_dir=$(sed "s+data+results-sat+g" <<<$dir)
   mkdir -p "$res_dir"
   for mode in LAB POL NNF_POL NNF_MUTEX_POL LABELNEG_POL; do
     python3 ../evaluate.py "$dir" -m $mode -o "$res_dir" --sat
