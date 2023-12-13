d729f94d2d2a683d18f7fa1f217d56bf35bda23d
Gabriele Masina <gabriele.masina@virgilio.it>
2023-12-13 09:26:01 +0100

    Fix: generate WMI benchmark as in UAI paper

--- benchmark/wmi/generate.sh
+++ benchmark/wmi/generate.sh
@@ -2,12 +2,25 @@
 
 source config.sh
 
-mkdir -p $DIR/data
-
-BOOL=10
-DEPTHS="3 5 7 9"
+mkdir -p $DIR/data $DIR/results
 N=10
+SEED=666
 
-for DEPTH in $DEPTHS; do
-  python3 generate.py -b "$BOOL" -r 0 -d "$DEPTH" -s 666 -o $DIR/data -n $N
+MIN_BOOL=3
+MAX_BOOL=3
+MIN_REAL=3
+MAX_REAL=3
+MIN_DEPTH=4
+MAX_DEPTH=7
+
+
+for ((bool=$MIN_BOOL; bool<=$MAX_BOOL; bool++))
+do
+	for ((real=$MIN_REAL; real<=$MAX_REAL; real++))
+	do
+		for ((depth=$MIN_DEPTH; depth<=$MAX_DEPTH; depth++))
+		do
+			python3 generate.py -b $bool -r $real -d $depth -o $DIR/data -n $N --seed $SEED
+		done
+	done
 done
